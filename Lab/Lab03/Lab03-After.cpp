825370b6489f40e618084a3a44ed499be2aa3bb7
Fee <feenix7248@gmail.com>
2025-07-16 01:12:24 +0700

    Update 16/7/68

--- Lab/Lab03/Lab03-After.cpp
+++ Lab/Lab03/Lab03-After.cpp
@@ -5,32 +5,32 @@ int isPrime(int num) {
     if( num < 2 ) 
     return 0 ;
     for ( int i = 2 ; i <= sqrt ( num ) ; i++ ) {
-        if ( num % i == 0 ) ;
+        if ( num % i == 0 ) 
         return 0 ;
     }
     return 1 ;
-}
+}//end function isPrime
 
 int main() {
     int n ;
     printf( "Enter N : " ) ;
     scanf( "%d" , &n ) ;
-    int arr[n] ; 
+    int arr[ n ] ; 
     for( int i = 0 ; i < n ; i++ ) {
-        printf( "Enter value[%d] : " , i ) ;
-        scanf( "%d" , &arr[i] ) ;
+        printf( "Enter value[ %d ] : " , i ) ;
+        scanf( "%d" , &arr[ i ] ) ;
     }//end for
     printf( "Index:  " ) ;
     for( int i = 0 ; i < n ; i++ ) {
-        printf( "%2d" , i ) ;
+        printf( "%2d " , i ) ;
     }//end for
     printf( "\n" ) ;
     printf( "Array:  " ) ;
     for( int i = 0 ; i < n ; i++ ) {
-        if( isPrime( arr[i] ) )
-        printf( "%2d" , arr[i] ) ; 
+        if( isPrime( arr[ i ] ) )
+        printf( "%2d " , arr[ i ] ) ; 
         else
-        printf( "%2s" , "#" ) ; 
+        printf( "%2s " , "#" ) ; 
     }//end for
     printf( "\n" ) ;
     return 0 ;
