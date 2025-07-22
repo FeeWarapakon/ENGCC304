39e9bd3d29af8d5c9fbacf7de2ab614f063a75eb
Fee <feenix7248@gmail.com>
2025-07-22 21:37:08 +0700

    Update

---
+++ Lab/Lab04/Lab04.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    char Emp_ID[10] ;
+    int hours ;
+    float Salary_amount_hours , Salary_amount ;
+
+    printf ("Employees ID: ") ;
+    scanf ("%10s", Emp_ID) ;
+
+    printf ("working hours: ") ;
+    scanf ("%d", &hours);
+
+    printf ("Salary_amount/hours: ");
+    scanf  ("%f" , &Salary_amount_hours );
+
+    Salary_amount = hours * Salary_amount_hours ;
+
+    printf ("Employees ID: %s\n", Emp_ID) ;
+    printf ("working hours: %f\n", Salary_amount) ;
+
+    return 0 ;  
+}//end main function
