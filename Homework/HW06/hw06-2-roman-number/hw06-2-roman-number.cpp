813e8656bf67e51a00d86467b620f92b46c9adda
Fee <feenix7248@gmail.com>
2025-08-18 17:04:53 +0700

    Update18/8/2568

--- Homework/HW06/hw06-2-roman-number/hw06-2-roman-number.cpp
+++ Homework/HW06/hw06-2-roman-number/hw06-2-roman-number.cpp
@@ -1,15 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    char Name[50] ;
-    int  Age ;
-    printf( "Enter your name: " );
-    scanf( "%s", Name);
-    printf( "Enter your age: " ) ;
-    scanf("%d", &Age);
-    printf("- - - - - -\n");
-    printf("Hello %s\n", Name); 
-    printf("Age = %d\n", Age); 
-    
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    char *symbols[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+    printf("%d = ", num);
+    for (int i = 0; i < 13; i++) {
+        int count = num / values[i]; // จำนวนครั้งที่ต้องแสดงสัญลักษณ์นี้
+        for (int j = 0; j < count; j++) {
+            printf("%s", symbols[i]);
+        }
+        num -= count * values[i];
+    }
+    printf("\n");
 
 }//end main function
