c557f9281f88052d4e23e0c78bc042292b00315e
Fee <feenix7248@gmail.com>
2025-10-01 21:01:38 +0700

    Update 01/10/2568

--- ComPro/ENGCC304 -Lab-1.1/ENGCC304 -Lab-1.1.cpp
+++ ComPro/ENGCC304 -Lab-1.1/ENGCC304 -Lab-1.1.cpp
@@ -4,6 +4,7 @@ int main() {
     int a, b, c ;
     int sum ;
 
+    printf( "Enter number : " ) ;
     scanf( "%d %d %d" , &a, &b, &c ) ;
     sum = a + b + c ;
     printf( "Answer = %d\n" , sum ) ; 
