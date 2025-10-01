fe138ff21c756e6b905e9737aaa2b1fae198eb2b
Fee <feenix7248@gmail.com>
2025-10-01 20:33:11 +0700

    Update 1/10/2568

---
+++ ComPro/ENGCC304 -Lab-1.1/ENGCC304 -Lab-1.1.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c ;
+    int sum ;
+
+    scanf( "%d %d %d" , &a, &b, &c ) ;
+    sum = a + b + c ;
+    printf( "Answer = %d\n" , sum ) ; 
+
+    return 0 ;
+}
