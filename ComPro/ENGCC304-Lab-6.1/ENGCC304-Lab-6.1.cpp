7d5de226b17c92a57850bc0bf36564bb56e3c50e
Fee <feenix7248@gmail.com>
2025-10-05 15:45:08 +0700

    Update 05/10/2568

---
+++ ComPro/ENGCC304-Lab-6.1/ENGCC304-Lab-6.1.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    int num ;
+    
+    printf( "User Input : " ) ;
+    if ( scanf( "%d" , &num ) != 1 ) {
+        printf( "Please enter a number only.\n" ) ;
+        return 1 ;
+    }
+
+    printf( "Result : " ) ;
+    switch( num ) {
+        case 1 :  printf( "one\n" ) ; break ;
+        case 2 :  printf( "two\n" ) ; break ;
+        case 3 :  printf( "three\n" ) ; break ;
+        case 4 :  printf( "four\n" ) ; break ;
+        case 5 :  printf( "five\n" ) ; break ;
+        case 6 :  printf( "six\n" ) ; break ;
+        case 7 :  printf( "seven\n" ) ; break ;
+        case 8 :  printf( "eight\n" ) ; break ;
+        case 9 :  printf( "nine\n" ) ; break ;
+        case 10 : printf( "ten\n" ) ; break ;
+        case 11 : printf( "eleven\n" ) ; break ;
+        case 12 : printf( "twelve\n" ) ; break ;
+        case 13 : printf( "thirteen\n" ) ; break ;
+        case 14 : printf( "fourteen\n" ) ; break ;
+        case 15 : printf( "fifteen\n" ) ; break ;
+        case 16 : printf( "sixteen\n" ) ; break ;
+        case 17 : printf( "seventeen\n" ) ; break ;
+        case 18 : printf( "eighteen\n" ) ; break ;
+        case 19 : printf( "nineteen\n" ) ; break ;
+        default : printf( "Number out of range!\n" ) ;
+    }
+
+    return 0 ;
+}//end function
