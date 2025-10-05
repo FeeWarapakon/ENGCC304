51d28651bc41517cb8a4913cf1add6da5cec4bd1
Fee <feenix7248@gmail.com>
2025-10-05 14:53:37 +0700

    Update 05/10/2568

---
+++ ComPro/ENGCC304-Lab-5.1/ENGCC304-Lab-5.1.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    char ID[11] ;           
+    int workhrs ;          
+    float salaryPerHr ;       
+    float ttSalary ;     
+
+    printf( "Employees ID : " ) ;
+    scanf( "%s", ID ) ;
+
+    printf( "working hrs: " ) ;
+    scanf( "%d" , &workhrs ) ;
+
+    printf( "Salary amount/hr: " ) ;
+    scanf( "%f" , &salaryPerHr ) ;
+
+    ttSalary = workhrs * salaryPerHr ;
+
+    printf( "Employees ID = %s\n" , ID ) ;
+    printf( "Salary = U$ %.2f\n" , ttSalary ) ;
+
+    return 0 ;
+}// end function
