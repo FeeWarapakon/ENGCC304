bd156518d0d6df19037710564360d1b97bd97b23
Fee <feenix7248@gmail.com>
2025-10-06 18:21:17 +0700

    Update 06/10/2568

---
+++ ComPro/ENGCC304-Lab-9.1/ENGCC304-Lab-9.1.cpp
@@ -0,0 +1,47 @@
+ #include <stdio.h>
+ #include <math.h>
+
+int main () {
+    int N ;
+    printf( "Enter N : " ) ;
+    scanf("%d", &N) ;
+    
+    int value[N] ;
+    int Sqrt ;
+
+  
+
+    for ( int i = 0 ; i < N ; i++ ) {
+        printf( "Enter value[%d] : ", i ) ;
+        scanf( "%d", &value[i] ) ;   
+    } // end for
+
+    printf( "Index :\t" ) ;
+    for ( int i = 0 ; i < N ; i++ ) {
+        printf( "%d\t", i ) ;
+    } // end for
+    printf( "\n" ) ;
+
+    printf( "Array :\t" ) ;
+    for ( int i = 0 ; i < N ; i++ ) {
+        
+        if (value[i] % 2 == 1&& value[i] != 1 ) { // กรณีเลขคี่
+          
+            Sqrt = ( int )sqrt( value[i] ) ;
+            for ( int j = Sqrt ; j > 0 ; j-- ) {
+                if ( value[i] % j == 0 && j != 1 ) {
+                    printf( "#\t" ) ;
+                    break ;
+                } else if ( j == 2 ) { // กรณีเป็นจำนวนเฉพาะ
+                    printf( "%d\t" , value[i] ) ;
+                } // end if
+            } // end for
+        } else if (value[i] % 2 == 0 && value[i] != 2 ){ // กรณีเลขคู่
+            printf( "#\t" );
+        } else if ( value[i] == 2 ) { // กรณี 2
+            printf( "%d\t" , value[i] ) ;
+        }  // end if
+    } // end for
+
+    return 0 ;
+}// end function
