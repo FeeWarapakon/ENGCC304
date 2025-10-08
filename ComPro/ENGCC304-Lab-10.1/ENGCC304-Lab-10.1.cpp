581b7553c56b3eb3c73cb4a6ad1b878dc6626e68
Fee <feenix7248@gmail.com>
2025-10-08 20:33:10 +0700

    Update 08/10/2568

---
+++ ComPro/ENGCC304-Lab-10.1/ENGCC304-Lab-10.1.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main() {
+    char str [20] ;
+    char tempStr [20] ;
+    int N = 0 ;
+
+    printf( "Enter word : " ) ;
+    scanf( "%s" , str ) ;
+    strcpy( tempStr , str ) ;
+    N = strlen( str ) ;
+    int n = N ;
+
+    for( int i = 0 ; i < N ; i++ ){  // แปลงเป็นตัวพิมพ์เล็ก
+        tempStr[i] = tolower( tempStr [i] ) ; 
+        str [i] = tolower( tempStr [n-1] ) ;
+        n--;
+    }// end for
+    if( strcmp( tempStr , str ) == 0 ) {// เปรียบเทียบข้อความ
+        printf( "Pass." ) ;
+    } else{ 
+        printf( "Not Pass." ) ;
+    }// end if
+    return 0 ;
+} // end function
